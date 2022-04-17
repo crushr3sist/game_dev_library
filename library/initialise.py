7cf6acc37c9c6f8b742b158f5994066c1b4c14c8
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:15:25 +1000

    Update initialise.py

--- library/initialise.py
+++ library/initialise.py
@@ -1,18 +1,19 @@
 import pygame
+
+
 def initialise(set_print=bool, quit_func=bool):
     pygame.init()
-    ERROR = pygame.get_error()   
+    ERROR = pygame.get_error()
     SDL_VERSION = pygame.get_sdl_version()
     PYGAME_VERSION = pygame.version.ver
 
     if set_print == True:
-        print(  
-        f'''
+        print(
+            f"""
             SDL_VERSION : {SDL_VERSION}\n
             PYGAME_VERSION : {PYGAME_VERSION}\n
             
-        '''
+        """
         )
     else:
         pass
-
