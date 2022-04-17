db14bc12a6840ca31f95a508878d2e3863ef2828
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:14:43 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from library import * 
+from library import *
+
 
 def main():
     HEIGHT = 800
     WIDTH = 600
-    mainloop = MainLoop(WIDTH,HEIGHT,"title", "std")
+    mainloop = MainLoop(WIDTH, HEIGHT, "title", "std")
     rectangle = draw_rectangle(199, 199, 300, 400)
-    window= mainloop.window()
+    window = mainloop.window()
 
-    rectangle.draw(window,rectangle)
+    rectangle.draw(window, rectangle)
 
     mainloop.check_for_events()
-    
+
+
 if __name__ == "__main__":
     main()
