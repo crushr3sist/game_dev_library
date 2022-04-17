40e240c30e5a6dd4ad3d160e076aeac6b17abd36
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:16:04 +1000

    Update mainloop.py

--- library/mainloop.py
+++ library/mainloop.py
@@ -1,7 +1,8 @@
 from .initialise import *
 
-class MainLoop():
-    def __init__(self,width,height,title,runtype):
+
+class MainLoop:
+    def __init__(self, width, height, title, runtype):
         self.width = width
         self.height = height
         self.resolution = (self.width, self.height)
@@ -12,26 +13,19 @@ class MainLoop():
         elif self.runtype == "std":
             initialise(set_print=False)
 
-        
     def window(self):
         global screen
         pygame.display.init()
         screen = pygame.display.set_mode([self.width, self.height])
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
         return screen
 
-
     def check_for_events(self):
-        Running = True 
+        Running = True
         while Running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    Running = False 
+                    Running = False
                 initialise()
                 pygame.event.pump()
-
-
-
-
-
